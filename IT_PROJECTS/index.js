4e26f3eeccfbf05a3d96522a2cfbfe3de2788ef8
dharmalingam1310 <134496723+dharmalingam1310@users.noreply.github.com>
2023-09-29 12:29:22 +0530

    Add files via upload

---
+++ IT_PROJECTS/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const container = document.getElementById('sandy');
+const root = ReactDOM.createRoot(container);
+root.render(<p>hi how are you</p>
+<div>hi guys</div>
+);
